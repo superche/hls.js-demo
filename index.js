912d1fa4b3e77d8595cd8cd3ef0798bb32dd88f2
superche <35420957+superche@users.noreply.github.com>
2023-04-14 11:42:07 +0800

    refactor: update player

--- index.js
+++ index.js
@@ -3,16 +3,49 @@ import './style.css';
 
 // Write Javascript code!
 import Hls from 'hls.js';
-import plyr from 'plyr';
-var video = document.querySelector('#player');
+import Plyr from 'plyr';
 
-if (Hls.isSupported()) {
-  var hls = new Hls();
-  hls.loadSource('https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8');
+const VIDEO_SOURCE = 'https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8';
+
+function main() {
+  const video = document.querySelector('#player');
+  setupPlayer(video);
+  setupHls();
+
+  if (Hls.isSupported()) {
+    loadHlsVideo(video);
+    return;
+  } else {
+    loadBaseVideo(video);
+  }
+}
+
+function setupPlayer(video) {
+  const player = new Plyr(video, {
+    captions: {
+      active: true,
+      update: true,
+      language: 'en',
+    },
+  });
+  window.player = player;
+}
+
+function setupHls() {
+  window.hls = Hls;
+}
+
+function loadBaseVideo(video) {
+  video.src = VIDEO_SOURCE;
+}
+function loadHlsVideo(video) {
+  const hls = new Hls();
+  hls.loadSource(VIDEO_SOURCE);
   hls.attachMedia(video);
-  hls.on(Hls.Events.MANIFEST_PARSED, function () {
-    video.play();
+
+  window?.player?.on('languagechange', () => {
+    setTimeout(() => (hls.subtitleTrack = window.player.currentTract), 50);
   });
 }
 
-plyr.setup(video);
+main();
